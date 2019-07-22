c3711461615be69197d304daab15fe26c07816db
ksh <wm167@yandex.ru>
2019-07-22 16:54:58 +0300

    Add basic movement

--- engine.py
+++ engine.py
@@ -12,15 +12,28 @@ def main():
 
     tcod.console_set_custom_font('arial10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
     tcod.console_init_root(screen_width, screen_height, game_name, False, tcod.RENDERER_SDL2, 'F', True)
+    con = tcod.console_new(screen_width, screen_height)
 
     while True:
-        tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)
+        tcod.console_put_char(con, player_x, player_y, '@', tcod.BKGND_NONE)
+        tcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         tcod.console_flush()
+
+        tcod.console_put_char(con, player_x, player_y, ' ', tcod.BKGND_NONE)
 
         for event in tcod.event.wait():
             action = event_handler.handle(event)
             if action.get('exit'):
                 raise SystemExit()
+
+            move = action.get('move')
+            if move:
+                dx, dy = move
+                player_x += dx
+                player_y += dy
+
+            if action.get('fullscreen'):
+                tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
 
 
 
--- event_handler.py
+++ event_handler.py
@@ -7,6 +7,16 @@ def handle(event):
     elif event.type == 'KEYDOWN':
         if event.sym == tcod.event.K_ESCAPE:
             return {'exit': True}
-        elif event.sym == tcod.event.K_UP:
+
+        if event.sym == tcod.event.K_UP:
             return {'move': (0, -1)}
+        elif event.sym == tcod.event.K_DOWN:
+            return {'move': (0, 1)}
+        elif event.sym == tcod.event.K_LEFT:
+            return {'move': (-1, 0)}
+        elif event.sym == tcod.event.K_RIGHT:
+            return {'move': (1, 0)}
+
+        if event.sym == tcod.event.K_RETURN and (tcod.event.KMOD_LALT | tcod.event.KMOD_RALT):
+            return {'fullscreen': True}
     return {}
